84b0babaff7900b86fe0907ad40b325f0083cffe
Javicompi <javicompi@gmail.com>
2021-02-23 11:17:16 +0100

    Update Constants.kt

--- app/src/main/java/com/example/android/weatherme/utils/Constants.kt
+++ app/src/main/java/com/example/android/weatherme/utils/Constants.kt
@@ -4,7 +4,7 @@ object Constants {
 
     const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
 
-    const val API_KEY = "cd1f36365e0469502044b35caaf1cdde"
+    const val API_KEY = ""
 
     const val REQUEST_LOCATION_PERMISSION_CODE = 33
 
