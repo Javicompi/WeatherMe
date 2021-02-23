70dff641c7f91dff4092394b5dc77984749046f4
javicompi <javicompi@gmail.com>
2021-02-23 14:12:23 +0100

    Hidding API key

--- app/src/main/java/com/example/android/weatherme/data/database/CurrentWeatherDao.kt
+++ app/src/main/java/com/example/android/weatherme/data/database/CurrentWeatherDao.kt
@@ -6,6 +6,7 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.android.weatherme.data.database.entities.current.CurrentEntity
+import com.example.android.weatherme.data.network.api.Result
 
 @Dao
 interface CurrentWeatherDao {
