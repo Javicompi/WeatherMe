5cd2d76af223fc62669c9e0492803eb3865ede62
javicompi <javicompi@gmail.com>
2021-02-26 18:50:59 +0100

    Implemented Parcelize

--- app/src/main/java/com/example/android/weatherme/data/database/entities/current/CurrentEntity.kt
+++ app/src/main/java/com/example/android/weatherme/data/database/entities/current/CurrentEntity.kt
@@ -4,8 +4,10 @@ import android.os.Parcel
 import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
 
 
+@Parcelize
 @Entity(tableName = "currents")
 data class CurrentEntity(
         val latitude: Double,
@@ -23,10 +25,10 @@ data class CurrentEntity(
         val windSpeed: Int,
         val windDegrees: Int,
         val clouds: Int,
-        val rainOneHour: Int = 0,
-        val rainThreeHours: Int = 0,
-        val snowOneHour: Int = 0,
-        val snowThreeHours: Int = 0,
+        val rainOneHour: Double = 0.0,
+        val rainThreeHours: Double = 0.0,
+        val snowOneHour: Double = 0.0,
+        val snowThreeHours: Double = 0.0,
         val country: String?,
         val sunrise: Long,
         val sunset: Long,
@@ -36,78 +38,4 @@ data class CurrentEntity(
         val cityName: String?,
         val deltaTime: Long,
         val visibility: Int,
-) : Parcelable {
-    constructor(parcel: Parcel) : this(
-            parcel.readDouble(),
-            parcel.readDouble(),
-            parcel.readInt(),
-            parcel.readString(),
-            parcel.readString(),
-            parcel.readString(),
-            parcel.readInt(),
-            parcel.readInt(),
-            parcel.readInt(),
-            parcel.readInt(),
-            parcel.readInt(),
-            parcel.readInt(),
-            parcel.readInt(),
-            parcel.readInt(),
-            parcel.readInt(),
-            parcel.readInt(),
-            parcel.readInt(),
-            parcel.readInt(),
-            parcel.readInt(),
-            parcel.readString(),
-            parcel.readLong(),
-            parcel.readLong(),
-            parcel.readInt(),
-            parcel.readLong(),
-            parcel.readString(),
-            parcel.readLong(),
-            parcel.readInt()) {
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeDouble(latitude)
-        parcel.writeDouble(longitude)
-        parcel.writeInt(weatherId)
-        parcel.writeString(shortDescription)
-        parcel.writeString(description)
-        parcel.writeString(icon)
-        parcel.writeInt(temp)
-        parcel.writeInt(tempMin)
-        parcel.writeInt(tempMax)
-        parcel.writeInt(feelsLike)
-        parcel.writeInt(pressure)
-        parcel.writeInt(humidity)
-        parcel.writeInt(windSpeed)
-        parcel.writeInt(windDegrees)
-        parcel.writeInt(clouds)
-        parcel.writeInt(rainOneHour)
-        parcel.writeInt(rainThreeHours)
-        parcel.writeInt(snowOneHour)
-        parcel.writeInt(snowThreeHours)
-        parcel.writeString(country)
-        parcel.writeLong(sunrise)
-        parcel.writeLong(sunset)
-        parcel.writeInt(timeZone)
-        parcel.writeLong(cityId)
-        parcel.writeString(cityName)
-        parcel.writeLong(deltaTime)
-        parcel.writeInt(visibility)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<CurrentEntity> {
-        override fun createFromParcel(parcel: Parcel): CurrentEntity {
-            return CurrentEntity(parcel)
-        }
-
-        override fun newArray(size: Int): Array<CurrentEntity?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+) : Parcelable
