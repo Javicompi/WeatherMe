5a396b417d1e04df1f838f68eba10e795d91f79f
javicompi <javicompi@gmail.com>
2021-03-03 10:32:32 +0100

    Removed unused code

--- app/src/main/java/com/example/android/weatherme/WeatherMeApplication.kt
+++ app/src/main/java/com/example/android/weatherme/WeatherMeApplication.kt
@@ -15,44 +15,12 @@ import javax.inject.Inject
 @HiltAndroidApp
 class WeatherMeApplication : Application(), Configuration.Provider {
 
-    //val applicationScope = CoroutineScope(Dispatchers.Default)
-
     @Inject
     lateinit var workerFactory: HiltWorkerFactory
-
-    /*override fun onCreate() {
-        super.onCreate()
-        delayedInit()
-    }*/
 
     override fun getWorkManagerConfiguration(): Configuration {
         return Configuration.Builder()
             .setWorkerFactory(workerFactory)
             .build()
     }
-
-    /*private fun delayedInit() {
-        applicationScope.launch {
-            setUpRefreshCurrentsWorker()
-        }
-    }
-
-    private fun setUpRefreshCurrentsWorker() {
-        val constraints = Constraints.Builder()
-            .setRequiredNetworkType(NetworkType.CONNECTED)
-            .setRequiresBatteryNotLow(true)
-            //.setRequiresDeviceIdle(true)
-            .build()
-
-        val repeatingRequest =
-            PeriodicWorkRequestBuilder<RefreshCurrentsWorker>(
-                Constants.PERIODIC_REQUEST_DELAY_MINS, TimeUnit.MINUTES
-            ).setConstraints(constraints).build()
-
-        WorkManager.getInstance(this).enqueueUniquePeriodicWork(
-            RefreshCurrentsWorker.WORK_NAME,
-            ExistingPeriodicWorkPolicy.KEEP,
-            repeatingRequest
-        )
-    }*/
 }
