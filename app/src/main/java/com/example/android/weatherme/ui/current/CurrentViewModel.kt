d28d91385d3d5738aba60178c473726b5d366100
javicompi <javicompi@gmail.com>
2021-03-04 18:26:09 +0100

    Removed unused code

--- app/src/main/java/com/example/android/weatherme/ui/current/CurrentViewModel.kt
+++ app/src/main/java/com/example/android/weatherme/ui/current/CurrentViewModel.kt
@@ -1,34 +1,19 @@
 package com.example.android.weatherme.ui.current
 
-import android.app.Application
-import android.util.Log
-import androidx.constraintlayout.widget.ConstraintSet
-import androidx.hilt.Assisted
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.*
 import com.example.android.weatherme.data.Repository
-import com.example.android.weatherme.data.database.entities.current.CurrentEntity
 import com.example.android.weatherme.utils.SingleLiveEvent
 import kotlinx.coroutines.launch
 
 class CurrentViewModel @ViewModelInject constructor(
-        private val repository: Repository,
-        @Assisted private val savedStateHandle: SavedStateHandle
+        private val repository: Repository
 ) : ViewModel() {
-
-    init {
-        Log.d("CurrentViewModel", "test")
-    }
 
     val showSnackBar: SingleLiveEvent<String> = SingleLiveEvent()
     val showSnackBarInt: SingleLiveEvent<Int> = SingleLiveEvent()
     val loadNewCurrent: SingleLiveEvent<Long> = SingleLiveEvent()
-    //val loadNewCurrent: MutableLiveData<Long> = MutableLiveData(0L)
 
-    /*val currentSelected: LiveData<CurrentEntity> = savedStateHandle.getLiveData<Long>("cityId").switchMap { cityId ->
-        Log.d("CurrentViewModel", "cityId: $cityId")
-        repository.getCurrentByKey(cityId)
-    }*/
     val currentSelected = loadNewCurrent.switchMap { cityId ->
         repository.getCurrentByKey(cityId)
     }
