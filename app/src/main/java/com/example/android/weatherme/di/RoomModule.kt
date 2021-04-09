f5614aa60e8694f42750f0151a62e49baaa66b7c
javicompi <javicompi@gmail.com>
2021-04-09 11:03:49 +0200

    Added RoomModule.kt for DI

---
+++ app/src/main/java/com/example/android/weatherme/di/RoomModule.kt
@@ -0,0 +1,32 @@
+package com.example.android.weatherme.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.android.weatherme.data.database.CurrentWeatherDao
+import com.example.android.weatherme.data.database.WeatherDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Singleton
+
+@InstallIn(ApplicationComponent::class)
+@Module
+class RoomModule {
+
+    @Singleton
+    @Provides
+    fun provideDatabase(@ApplicationContext context: Context): WeatherDatabase {
+        return Room.databaseBuilder(
+                context,
+                WeatherDatabase::class.java,
+                "Weather.db"
+        ).build()
+    }
+
+    @Provides
+    fun provideCurrentWeatherDao(database: WeatherDatabase): CurrentWeatherDao {
+        return database.currentWeatherDao()
+    }
+}
