3e126c2905caec33663aa1b909977db55823e4bc
javicompi <javicompi@gmail.com>
2021-04-09 11:12:36 +0200

    Added RepositoryModule.kt for DI

---
+++ app/src/main/java/com/example/android/weatherme/di/RepositoryModule.kt
@@ -0,0 +1,34 @@
+package com.example.android.weatherme.di
+
+import com.example.android.weatherme.data.Repository
+import com.example.android.weatherme.data.database.CurrentWeatherDao
+import com.example.android.weatherme.data.network.api.WeatherApiService
+import com.example.android.weatherme.utils.PreferencesHelper
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import javax.inject.Singleton
+
+@InstallIn(ApplicationComponent::class)
+@Module
+object RepositoryModule {
+
+    @Singleton
+    @Provides
+    fun provideRepository(
+            @IoDispatcher
+            dbDispatcher: CoroutineDispatcher,
+            currentWeatherDao: CurrentWeatherDao,
+            weatherApiService: WeatherApiService,
+            preferencesHelper: PreferencesHelper
+    ): Repository {
+        return Repository(
+                dbDispatcher,
+                currentWeatherDao,
+                weatherApiService,
+                preferencesHelper
+        )
+    }
+}
