8aa6898781cc68b6c90ed34af223a8c6d69dd889
javicompi <javicompi@gmail.com>
2021-04-09 11:07:22 +0200

    Added RetrofitModule.kt for DI

---
+++ app/src/main/java/com/example/android/weatherme/di/RetrofitModule.kt
@@ -0,0 +1,30 @@
+package com.example.android.weatherme.di
+
+import com.example.android.weatherme.data.network.api.WeatherApiService
+import com.example.android.weatherme.utils.Constants
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@InstallIn(ApplicationComponent::class)
+@Module
+object RetrofitModule {
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(): Retrofit.Builder {
+        return Retrofit.Builder()
+                .addConverterFactory(MoshiConverterFactory.create())
+                .baseUrl(Constants.BASE_URL)
+    }
+
+    @Singleton
+    @Provides
+    fun provideWeatherApiService(retrofit: Retrofit.Builder): WeatherApiService {
+        return retrofit.build().create(WeatherApiService::class.java)
+    }
+}
