36e3f6189f8ab5311ba49355b04fe37f167a2259
javicompi <javicompi@gmail.com>
2021-04-09 11:09:29 +0200

    Added DispatcherModule.kt for DI

---
+++ app/src/main/java/com/example/android/weatherme/di/DispatcherModule.kt
@@ -0,0 +1,22 @@
+package com.example.android.weatherme.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Qualifier
+
+@InstallIn(ApplicationComponent::class)
+@Module
+object DispatcherModule {
+
+    @IoDispatcher
+    @Provides
+    fun provideIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
+}
+
+@Retention(AnnotationRetention.BINARY)
+@Qualifier
+annotation class IoDispatcher
