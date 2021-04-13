4894dc20de290f6551453e63df04a3a57444c80e
javicompi <javicompi@gmail.com>
2021-04-13 17:11:37 +0200

    Added the Moshi adapter and removed unused calls

--- app/src/main/java/com/example/android/weatherme/data/network/api/WeatherApiService.kt
+++ app/src/main/java/com/example/android/weatherme/data/network/api/WeatherApiService.kt
@@ -4,8 +4,9 @@ import com.example.android.weatherme.data.network.models.ErrorResponse
 import com.example.android.weatherme.data.network.models.current.Current
 import com.example.android.weatherme.utils.Constants
 import com.haroldadmin.cnradapter.NetworkResponse
-import com.haroldadmin.cnradapter.NetworkResponseAdapter
 import com.haroldadmin.cnradapter.NetworkResponseAdapterFactory
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
@@ -13,8 +14,12 @@ import retrofit2.http.Query
 import java.util.*
 
 
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
 private val retrofit = Retrofit.Builder()
-        .addConverterFactory(MoshiConverterFactory.create())
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
         .addCallAdapterFactory(NetworkResponseAdapterFactory())
         .baseUrl(Constants.BASE_URL)
         .build()
@@ -22,32 +27,25 @@ private val retrofit = Retrofit.Builder()
 interface WeatherApiService {
 
     @GET("weather?appid=${Constants.API_KEY}")
-    suspend fun getCurrentWeatherByName(
-            @Query("q") location: String,
-            @Query("units") units: String? = "metric",
-            @Query("lang") language: String = Locale.getDefault().toString().subSequence(0, 2).toString()
-    ): Current
-
-    @GET("weather?appid=${Constants.API_KEY}")
-    suspend fun getCurrentWeatherByLatLon(
-            @Query("lat") latitude: Double,
-            @Query("lon") longitude: Double,
-            @Query("units") units: String? = "metric",
-            @Query("lang") language: String = Locale.getDefault().toString().subSequence(0, 2).toString()
-    ): Current
-
-    @GET("weather?appid=${Constants.API_KEY}")
-    suspend fun getCurrentWeatherById(
+    suspend fun getCurrentResponseById(
             @Query("id") id: Long,
             @Query("units") units: String? = "metric",
-            @Query("lang") language: String = Locale.getDefault().toString().subSequence(0, 2).toString()
-    ): Current
+            @Query("lang") language: String = Locale.getDefault().language
+    ): NetworkResponse<Current, ErrorResponse>
 
     @GET("weather?appid=${Constants.API_KEY}")
     suspend fun getCurrentResponseByName(
             @Query("q") location: String,
             @Query("units") units: String? = "metric",
-            @Query("lang") language: String = Locale.getDefault().toString().subSequence(0, 2).toString()
+            @Query("lang") language: String = Locale.getDefault().language
+    ): NetworkResponse<Current, ErrorResponse>
+
+    @GET("weather?appid=${Constants.API_KEY}")
+    suspend fun getCurrentResponseByLatLon(
+            @Query("lat") latitude: Double,
+            @Query("lon") longitude: Double,
+            @Query("units") units: String? = "metric",
+            @Query("lang") language: String = Locale.getDefault().language
     ): NetworkResponse<Current, ErrorResponse>
 }
 
