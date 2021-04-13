a77122de763bc5a09419ef443f80b59ce4145f3e
javicompi <javicompi@gmail.com>
2021-04-13 17:42:16 +0200

    Created HourlyEntity.kt

---
+++ app/src/main/java/com/example/android/weatherme/data/network/models/perhour/HourlyEntity.kt
@@ -0,0 +1,35 @@
+package com.example.android.weatherme.data.network.models.perhour
+
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+@Entity(tableName = "hourlys")
+data class HourlyEntity(
+        @PrimaryKey(autoGenerate = true)
+        val id: Long?,
+        val cityId: Long,
+        val deltaTime: Long,
+        val offset: Int,
+        val temp: Int,
+        val feelsLike: Int,
+        val pressure: Int,
+        val humidity: Int,
+        val dewPoint: Double,
+        val uvi: Double,
+        val clouds: Int,
+        val visibility: Int,
+        val windSpeed: Int,
+        val windDegrees: Int,
+        val pop: Double,
+        val rainOneHour: Double = 0.0,
+        val rainThreeHours: Double = 0.0,
+        val snowOneHour: Double = 0.0,
+        val snowThreeHours: Double = 0.0,
+        val weatherId: Int,
+        val shortDescription: String,
+        val description: String,
+        val icon: String,
+) : Parcelable
