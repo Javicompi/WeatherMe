1764373463e6ac47243b2d3d00f50188c8e5b806
Javicompi <javicompi@gmail.com>
2021-04-16 18:51:33 +0200

    Removed PerHourWithHourly.kt and PerHourEntity.kt

---
+++ app/src/main/java/com/example/android/weatherme/data/database/HourlyDao.kt
@@ -0,0 +1,27 @@
+package com.example.android.weatherme.data.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.example.android.weatherme.data.database.entities.perhour.HourlyEntity
+
+@Dao
+interface HourlyDao {
+
+    @Query("SELECT * FROM hourlys WHERE `cityId` = :id")
+    fun getHourlysByKey(id: Long): LiveData<List<HourlyEntity>>
+
+    @Query("SELECT * FROM hourlys WHERE `cityId` =:id")
+    fun getRawHourlysByKey(id: Long): List<HourlyEntity>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertHourlys(hourlys: List<HourlyEntity>): List<Long>
+
+    @Query("DELETE FROM hourlys WHERE `cityId` = :id")
+    fun deleteHourlys(id: Long)
+
+    @Transaction
+    fun updateHourlysByKey(hourlys: List<HourlyEntity>) {
+        deleteHourlys(hourlys[0].cityId)
+        insertHourlys(hourlys)
+    }
+}
