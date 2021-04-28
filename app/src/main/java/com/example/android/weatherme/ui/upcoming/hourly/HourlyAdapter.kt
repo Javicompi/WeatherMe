ebd9ff9aa976b6c86534c65dfd0112655c08c7fb
Javicompi <javicompi@gmail.com>
2021-04-28 11:51:17 +0200

    Expandable hourly item behaviour implemented

--- app/src/main/java/com/example/android/weatherme/ui/upcoming/hourly/HourlyAdapter.kt
+++ app/src/main/java/com/example/android/weatherme/ui/upcoming/hourly/HourlyAdapter.kt
@@ -1,18 +1,20 @@
 package com.example.android.weatherme.ui.upcoming.hourly
 
+import android.graphics.drawable.AnimatedVectorDrawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.example.android.weatherme.R
 import com.example.android.weatherme.data.database.entities.perhour.HourlyEntity
 import com.example.android.weatherme.databinding.ListItemHourlyBinding
 
 class HourlyAdapter : ListAdapter<HourlyEntity, HourlyAdapter.ViewHolder>(HourlyDiffCallback()) {
 
-    private var expandedPosition = -1
-    private var prevExpandedPosition = -1
+    private var expandedPosition = RecyclerView.NO_POSITION
+    private var prevExpandedPosition = RecyclerView.NO_POSITION
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder.from(parent)
@@ -21,13 +23,22 @@ class HourlyAdapter : ListAdapter<HourlyEntity, HourlyAdapter.ViewHolder>(Hourly
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val isExpanded = position == expandedPosition
         holder.binding.listItemHourlyDetails.visibility = if (isExpanded) View.VISIBLE else View.GONE
-        holder.itemView.isActivated = isExpanded
-        if (isExpanded) prevExpandedPosition = position
+        if (isExpanded) {
+            prevExpandedPosition = position
+            holder.binding.listItemHourlyArrow.setImageResource(R.drawable.ic_arrow_up_down)
+        } else {
+            holder.binding.listItemHourlyArrow.setImageResource(R.drawable.ic_arrow_down_up)
+        }
         holder.itemView.setOnClickListener {
             expandedPosition = if (isExpanded) -1 else position
+            if (isExpanded) {
+                holder.binding.listItemHourlyArrow.setImageResource(R.drawable.ic_arrow_up_down)
+            } else {
+                holder.binding.listItemHourlyArrow.setImageResource(R.drawable.ic_arrow_down_up)
+            }
             notifyItemChanged(prevExpandedPosition)
             notifyItemChanged(expandedPosition)
-
+            (holder.binding.listItemHourlyArrow.drawable as AnimatedVectorDrawable).start()
         }
         holder.bind(getItem(position))
     }
